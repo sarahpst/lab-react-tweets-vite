b4c175cd3c76580a6615c85b6771eb1a6e0f2b74
sarahpst <111440718+sarahpst@users.noreply.github.com>
2023-05-08 21:37:50 +0200

    done

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,34 +1,22 @@
-function Tweet() {
+import ProfileImage from "./ProfileImage.jsx";
+import User from "./User.jsx";
+import Timestamp from "./Timestamp.jsx";
+import Message from "./Message.jsx";
+import Actions from "./Actions.jsx";
+function Tweet({ tweet }) {
   return (
     <div className="tweet">
-      <img
-        src="https://i.imgur.com/9yw1Fyw.jpg"
-        className="profile"
-        alt="profile"
-      />
+      <ProfileImage image={tweet.user.image} />
 
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
-
-          <span className="timestamp">Nov 30, 2020</span>
+          <User userData={tweet.user} />
+          <Timestamp time={tweet.timestamp} />
         </div>
 
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
+        <Message message={tweet.message} />
 
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment"></i>
-          <i className="fas fa-retweet"></i>
-          <i className="far fa-heart"></i>
-          <i className="fas fa-share"></i>
-        </div>
+        <Actions />
       </div>
 
       <i className="fas fa-ellipsis-h"></i>
